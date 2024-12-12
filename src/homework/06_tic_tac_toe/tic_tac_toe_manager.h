e3c883e7d3d5161d78c0a405b2fa626e1ace7392
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-12-12 15:10:22 -0600

    Update tic_tac_toe_manager.h

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,22 @@
 //h
+
+#define tic_tac_toe_manager_H
+
+#include "tic_tac_toe.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+class TicTacToeManager {
+private:
+    std::vector<std::unique_ptr<TicTacToe>> games;
+    int x_win{0};
+    int o_win{0};
+    int ties{0};
+    void update_winner_count(std::string vinner);
+
+public:
+    void save_game(std::unique_ptr<TicTacToe> game);
+    void get_winner_totals(int& ,int& ,int&);
+    void display_games();
+};
