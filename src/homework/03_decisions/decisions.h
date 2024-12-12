8500a008511b5d94259619ea48237b736d9da12f
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-12-12 13:38:39 -0600

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,13 @@
 //write include required statements
+#include <iostream>
 
+//write a function(s) prototype here
 
-//Write a function(s) prototype  here
+std::string get_letter_grade_using_if(int grade);
+std::string get letter_grade_using_switch(int grade);
 
+//support function, range funcion from python. 
+bool in_range(int grade, int low, int high);
 
 
 
