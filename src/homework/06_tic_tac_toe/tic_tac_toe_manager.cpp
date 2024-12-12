7a8f7dcad1f3f3c09ecc20a5a5d985bd959afca5
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-12-12 15:10:09 -0600

    Update tic_tac_toe_manager.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,42 @@
 //cpp
+
+#include "tic_tac_toe_manager.h"
+#include <memory>
+
+
+#include <iostream>
+using std::cout;
+ 
+void TicTacToeManager::save_game(std::unique_ptr<TicTacToe> game) {
+    update_winner_count(game->get_winner());
+    games.push_back(std::move(game));
+    // We don't have clear winner determiner lol
+}
+
+void TicTacToeManager::display_games() {
+    for(auto& game : games) {
+        (*game).display_board();
+        cout << "\n";
+    }
+}
+
+void TicTacToeManager::update_winner_count(std::string winner){
+    cout << "Debug - Winner is: " << winner << "\n"; // temp, it is opposite of real winner lol
+    if (winner == "X" || winner == "x"){
+        x_win ++;
+    }
+
+    else if (winner == "O" || winner == "o"){
+        o_win ++;
+    }
+
+    else if (winner == "C" || winner == "c"){
+        ties ++;
+    }
+}
+
+void TicTacToeManager::get_winner_totals(int& o, int& w, int& t){
+    o = o_win;
+    w = x_win;
+    t = ties;
+}
