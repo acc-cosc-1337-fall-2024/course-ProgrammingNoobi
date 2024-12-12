d1fd0871e594694fc3c7b31947e0d9571cd46df2
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-12-12 13:27:53 -0600

    Update hwexpressions.h

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,23 @@
-﻿//example
-int add_numbers(int num1, int num2);
+//capital Management System by Geekware 
+//Declare functions first.
+//Bills restraunt marketing department asked to add tip based+tax price, not on base meal alone.
 
-//write function prototype here
+//we will simulate customers
 
+double get_order();
 
+//tax the customer
 
+double add_tax(double base_price);
 
+//enforce unreasoned tips.
+
+double tip();
+
+//sum it up
+
+double add_tip(double base_price, double add_tip);
+
+//capital management system by geekware 
+//Declare functions first.
+//Bills restraunt marketing department asked to add tip based+tax price, not on base meal alone.
