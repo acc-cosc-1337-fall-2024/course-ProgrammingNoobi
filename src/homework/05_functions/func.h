f7a348f7807f3a54342dd90b184094129102bf38
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-12-12 15:05:06 -0600

    Update func.h

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,18 @@
 //add function(s) prototype here
+#include<string>
+
+using std::string;
+
+// Functional functions
+
+double get_gc_content(const string& dna);
+
+string get_dna_compliment(string dna);
+
+string reverse_string(string dna);
+
+// Menu functionality 
+
+int user_input();
+
+void manager(int num);
